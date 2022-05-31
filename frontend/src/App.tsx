801f22de0d021ebac873de5ea7a82873eeb46caf
Thomas S <tsauques@vectice.com>
2022-05-31 09:36:07 +0200

    Add list to App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,72 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { PAGE_SIZE, WORKSPACE_ID_LIST } from './constants';
 import { GET_PROJECT_LIST } from './getProjectList';
+import { useQuery } from '@apollo/client';
+
+import styles from './App.css';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const { data, loading, refetch } = useQuery(GET_PROJECT_LIST, {
+    fetchPolicy: 'cache-only',
+    variables: {
+      workspaceIdList: WORKSPACE_ID_LIST,
+      page: {
+        size: PAGE_SIZE,
+        index: 1,
+      },
+    },
+  });
+
+  useEffect(() => {
+    refetch({
+      workspaceIdList: WORKSPACE_ID_LIST,
+      page: {
+        size: PAGE_SIZE,
+        index: currentPage,
+      },
+    });
+  }, [currentPage]);
+
+  const nbOfPage = new Array(Math.floor((data?.getProjectList.total || 0) / PAGE_SIZE)).map((_, i) => i + 1);
+
+  if (loading) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div data-testid="project-title" className={styles.title}>Project</div>
+        </header>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        <div data-testid="project-title" className={styles.title}>Project</div>
+        {data?.getProjectList.items && (
+          <ul>
+            {data?.getProjectList.items.map((item) => (
+              <a href={`/project/${item.id}`}>
+                <li>
+                  <div>{item.name}</div>
+                  <div>{item.description}</div>
+                  <div>Last edited {item.updatedDate}</div>
+                </li>
+              </a>
+            ))}
+          </ul>
+        )}
       </header>
+      <div className={styles.pagination}>
+        {nbOfPage.map((page, index) => (
+          <div data-testid={`page-${index + 1}`} onClick={() => setCurrentPage(page)}>{index + 1}</div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- frontend/src/getProjectList.ts
+++ frontend/src/getProjectList.ts
@@ -1,4 +1,29 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const GET_PROJECT_LIST = gql`
+  query getProjectList(
+    $workspaceIdList: [Float!]!
+    $page: PageInput
+  ) {
+    getProjectList(
+      workspaceIdList: $workspaceIdList
+      page: $page
+    ) {
+      items {
+        id
+        name
+        description
+        updatedDate
+        documentationPages {
+          id
+          status
+        }
+      }
+      page {
+        index
+        size
+      }
+      total
+    }
+  }
 `;
