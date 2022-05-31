6ff8572a8fcc2f1bd461dc42975cc6d8709d2c71
Thomas S <tsauques@vectice.com>
2022-05-31 09:14:16 +0200

    chore: setup for PR

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -10,48 +10,4 @@ import { GET_PROJECT_LIST } from './getProjectList';
 import App from './App';
 
 describe('Page - Workspace Projects', () => {
-  const mocks: MockedResponse[] = [
-    getProjectListTestData(GET_PROJECT_LIST, {
-      workspaceIdList: [2],
-      page: {
-        size: 5,
-        index: 1,
-      },
-    }),
-    getProjectListTestData(GET_PROJECT_LIST, {
-      workspaceIdList: [2],
-      page: {
-        size: 5,
-        index: 2,
-      },
-    }),
-  ];
-
-  it('should display the list of projects with pagination', async () => {
-    render(
-      <MockedProvider
-        mocks={mocks}
-        defaultOptions={{
-          watchQuery: { fetchPolicy: 'no-cache' },
-          query: { fetchPolicy: 'no-cache' },
-        }}
-      >
-        <App />
-      </MockedProvider>,
-    );
-
-    screen.getByRole('heading', { name: /Projects/i });
-
-    await screen.findByRole<HTMLUListElement>('list');
-    const listItems = screen.getAllByRole<HTMLLIElement>('listitem');
-    expect(listItems.length).toBe(5);
-
-    const page2 = screen.getByRole<HTMLButtonElement>('button', { name: /2/i });
-
-    userEvent.click(page2);
-
-    await screen.findByRole<HTMLUListElement>('list');
-    const listItems2 = screen.getAllByRole<HTMLLIElement>('listitem');
-    expect(listItems2.length).toBe(2);
-  });
 });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* ADD CODE HERE */}
       </header>
     </div>
   );
